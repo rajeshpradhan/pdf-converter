54c16f638bf37d01de02b8d6c919f7ab458c670d
Rajesh Pradhan <rajesh@esharesinc.com>
2013-06-13 09:38:55 -0700

    print replaced with logging

--- worker.py
+++ worker.py
@@ -2,13 +2,15 @@
 import pika
 import time
 import os
+import logging
+
 
 connection = pika.BlockingConnection(pika.ConnectionParameters(
         host='localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='task_queue', durable=True)
-print ' [*] Waiting for messages. To exit press CTRL+C'
+logging.info(' [*] Waiting for messages. To exit press CTRL+C')
 if os.uname()=="Darwin":
     HTMLTOPDF = './wkhtmltopdf.mac '
 else:
@@ -19,14 +21,13 @@ def convert_to_pdf(fn):
         os.popen(HTMLTOPDF+" static/htmls/"+fn+".html static/pdfs/"+fn+".pdf")
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
+    logging.info(" [x] Received %r" % (body,))
     fn = body.split('.html')[0]
     cb = body.split('|')[1]
-    print fn
     convert_to_pdf(fn)
     #os.popen("wget http://localhost:8000/pdfrequester/upload/download/?pdffile=http://localhost:8000/static/"+fn+".pdf")
     os.popen("wget http://localhost:8000/pdfrequester/upload/download/?pdffile="+cb+fn+".pdf")
-    print " [x] Done"
+    logging.info (" [x] Done")
     ch.basic_ack(delivery_tag = method.delivery_tag)
 
 channel.basic_qos(prefetch_count=1)
